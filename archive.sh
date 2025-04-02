ffc492dc49c99bda3e37cbbd224d8cad0a939f71
Quint Guvernator <quint@guvernator.net>
2025-04-02 20:16:13 +0200

    fix for macos

--- archive.sh
+++ archive.sh
@@ -5,9 +5,12 @@ set -e
 dir="${1:?root directory of cargo project}"
 
 env="env"
+tar="tar"
 if [ "$(uname)" = "Darwin" ]; then
     env="genv"
+    tar="tar --no-xattrs"
 fi
 
+
 $env -C "$dir" cargo vendor
-exec tar --exclude-vcs --exclude="./prob-*.pdf" --exclude="./target" --exclude="./samples-*" --exclude="./*.DS_Store" --exclude="./.vscode" -cavf "$dir.tar.xz" "$dir"
+exec $tar --exclude-vcs --exclude="./prob-*.pdf" --exclude="./target" --exclude="./samples-*" --exclude="./*.DS_Store" --exclude="./.vscode" -cavf "$dir.tar.xz" "$dir"
